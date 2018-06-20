5ebea8a1a055c219130170bac78ff46839a0d816
杨俊艺 <1209032479@qq.com>
2018-06-20 08:02:55 +0800

    最初版

---
+++ CommandDemo/FormCommandDemo.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CommandDemo
+{
+    partial class FormCommandDemo
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_ExcProcedure = new System.Windows.Forms.Button();
+            this.Btn_Exc = new System.Windows.Forms.Button();
+            this.Btn_Count = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_ExcProcedure
+            // 
+            this.Btn_ExcProcedure.Location = new System.Drawing.Point(408, 248);
+            this.Btn_ExcProcedure.Name = "Btn_ExcProcedure";
+            this.Btn_ExcProcedure.Size = new System.Drawing.Size(160, 25);
+            this.Btn_ExcProcedure.TabIndex = 5;
+            this.Btn_ExcProcedure.Text = "执行存储过程DeleteOnLame";
+            this.Btn_ExcProcedure.UseVisualStyleBackColor = true;
+            this.Btn_ExcProcedure.Click += new System.EventHandler(this.Btn_ExcProcedure_Click);
+            // 
+            // Btn_Exc
+            // 
+            this.Btn_Exc.Location = new System.Drawing.Point(256, 247);
+            this.Btn_Exc.Name = "Btn_Exc";
+            this.Btn_Exc.Size = new System.Drawing.Size(125, 25);
+            this.Btn_Exc.TabIndex = 4;
+            this.Btn_Exc.Text = "执行带参数的SQL";
+            this.Btn_Exc.UseVisualStyleBackColor = true;
+            this.Btn_Exc.Click += new System.EventHandler(this.Btn_Exc_Click);
+            // 
+            // Btn_Count
+            // 
+            this.Btn_Count.Location = new System.Drawing.Point(160, 247);
+            this.Btn_Count.Name = "Btn_Count";
+            this.Btn_Count.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Count.TabIndex = 3;
+            this.Btn_Count.Text = "计算";
+            this.Btn_Count.UseVisualStyleBackColor = true;
+            this.Btn_Count.Click += new System.EventHandler(this.Btn_Count_Click);
+            // 
+            // FormCommandDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.Btn_ExcProcedure);
+            this.Controls.Add(this.Btn_Exc);
+            this.Controls.Add(this.Btn_Count);
+            this.Name = "FormCommandDemo";
+            this.Text = "CommandDemo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_ExcProcedure;
+        private System.Windows.Forms.Button Btn_Exc;
+        private System.Windows.Forms.Button Btn_Count;
+    }
+}
+
---
+++ CommandDemo/FormCommandDemo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CommandDemo
+{
+    public partial class FormCommandDemo : Form
+    {
+        public FormCommandDemo()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=pubs;Integrated Security=True;");
+
+        private void Btn_Count_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand sqlCommandCount = new SqlCommand
+            {
+                CommandText = "select AVG(price) from titles",
+                Connection = con
+            };
+            double result = Convert.ToDouble(sqlCommandCount.ExecuteScalar());
+            MessageBox.Show(result.ToString());
+            con.Close();
+        }
+
+        private void Btn_Exc_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand sqlCommandExc = new SqlCommand
+            {
+                CommandText = "select AVG(price) from titles where type=@type",
+                Connection = con
+            };
+            SqlParameter sqlParameterExc = new SqlParameter
+            {
+                ParameterName = "@type",
+                SqlDbType = SqlDbType.VarChar,
+                Value = "business"
+            };
+            sqlCommandExc.Parameters.Add(sqlParameterExc);
+            double result = Convert.ToDouble(sqlCommandExc.ExecuteScalar());
+            MessageBox.Show(result.ToString());
+            con.Close();
+        }
+
+        private void Btn_ExcProcedure_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand sqlCommandExcProcedure = new SqlCommand
+            {
+                CommandText = "DeleteOnLname",
+                Connection = con,
+                CommandType = CommandType.StoredProcedure
+            };
+            SqlParameter sqlParameterExcProcedure = new SqlParameter
+            {
+                ParameterName = "@Lname",
+                SqlDbType = SqlDbType.VarChar,
+                Value = "xxxx"
+            };
+            sqlCommandExcProcedure.Parameters.Add(sqlParameterExcProcedure);
+            double result = Convert.ToDouble(sqlCommandExcProcedure.ExecuteScalar());
+            MessageBox.Show(result.ToString() + "行被删除!");
+            con.Close();
+        }
+    }
+}
+//存储过程DeleteOnLname
+//--create procedure DeleteOnLname--匹配名字删除
+//--(
+//--	@lname varchar(40)
+//--)
+//--as
+//--delete from employee where lname=@lname
+//--return
