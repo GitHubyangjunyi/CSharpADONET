533828d7f63701226c1f84d8e5ad642055dfb551
杨俊艺 <1209032479@qq.com>
2018-06-20 13:15:06 +0800

    添加期末考试复习的一些名词解释和操作过程注释,版本回退点

--- DataAdapterDemo/FormDataAdapterDemo.cs
+++ DataAdapterDemo/FormDataAdapterDemo.cs
@@ -18,3 +18,31 @@ namespace DataAdapterDemo
         }
     }
 }
+//在ADO.NET编程中不能使用一个DataAdapter对象向多个DataTable填充数据
+//为了控制DataAdapter的Fill方法如何在填充数据之前向DataSet加载数据源架信息
+//可在调用该方法前将DataAdapter的MissingSchemaAction属性设为AddWithKey
+//使得Fill方法在填充数据前向DataSet架构添加额外的表和列并且给DataTable添加主键信息
+//为了提高性能,在使用DataAdapter填充DataSet前,可以将DataSet对象的EnforceConstraints属性值设为false
+//1.如何创建和配置DataAdapter为断开连接的应用程序提供对Microsoft SQL Server 2000数据库的只读访问？
+//解答：在代码中通过编程方式或使用数据适配器配置向导创建一个SqlDataAdapter对象
+//初始化DataAdapter的SelectCommand属性
+//必须指定SqlConnection对象,还必须指定一个从数据源检索数据的查询
+//要完成此操作可以定义一条SQL SELECT语句或者使用一个新的或现有的存储过程
+//2.使用DataAdapter填充DataSet的最有效方式是什么？
+//解答：首先创建一个与DataAdapter传入的数据结构相同的类型化DataSet
+//当准备填充DataSet时调用BeginLoadData方法来在加载数据的过程中禁用约束检查和索引维护
+//然后调用DataAdapter的Fill方法填充DataSet中的指定DataTable
+//最后在完全加载数据之后调用EndLoadData
+//3.如何配置DataAdapter以允许根据DataSet的内容更新数据源？
+//解答：当创建DataAdapter对象时为它的InsertCommand、UpdateCommand和DeleteCommand属性定义SqlCommand或OleDbCommand对象
+//DataAdapter隐式地使用这些命令对象将DataSet的更改传回数据源
+//4.如何将数据更改持久保存到数据源以及如何控制不同类型的更改进行持久保存的顺序？
+//解答：调用DataAdapter的FillSchema方法
+//如果DataSet中包含几个DataTable可能需要在其它操作之前先执行一些update操作以避免外键约束错误
+//在这种情况下调用DataSet的GetChanges方法,获得每个表上的更改(修改更新或删除)的子集
+//分别调用每个更改集上的Update方法以控制将更改持久保存到数据源的顺序
+//5.当在断开连接的应用程序中更新数据源时会出现什么类型的冲突？如何发现并解决这些冲突？
+//解答：断开连接应用程序使用开放式并发
+//就是说在应用程序与数据源断开连接时可能会有其他用户插入/更新或删除一些行,这样当试图将你的更改保存到数据源时就可能产生冲突
+//这种情况下会出现一个异常,可以捕获这个异常并通过检查DataSet、DataTable和DataRow的HasErrors属性来发现问题的位置
+//在定位了问题之后可以决定是拒绝提出的更改,强制对数据源进行更改,还是将更改在本地保存到DataSet中以便用户在以后保存更改
--- DataGridViewDemo/FormDataGridViewDemo.cs
+++ DataGridViewDemo/FormDataGridViewDemo.cs
@@ -18,3 +18,25 @@ namespace DataGridViewDemo
         }
     }
 }
+//DataTable对象用来表示DataSet中的表,一个DataTable表示内存中的关系数据的一个表
+//数据对于它所驻留的.NET应用程序是本地的,但它不能用现有的数据源填充
+//在DataTable中不能直接创建新行而是必须单独创建DataRow对象,并添加到DataTable的Rows集合中
+//若要向DataTable添加一个新行,首先要声明一个DataRow类型的变量
+//当前调用NewRow根据DataColumnCollection定义的表的结构来创建DataRow对象
+//向DataTable添加了新行之后可以使用索引或列名操作新行
+//在数据插入到新行后,使用Add方法将该行添加到DataRowCollection中
+//1.DataRow对象是什么？
+//解答：DataTable中的每一行都是通过DataRow对象呈现的，DataRow对象主要作为DataTable对象的Rows集合的一个元素而存在
+//从概念上看数据库行固有的链接到了某个给定表的结构,行与Rows集合的关系决定了该行的状态,行的状态信息存储在DataRow对象的RowState属性中
+//2.RowState和RowVersion属性有什么不同？
+//解答：RowState属性的值表示自从第一次创建DataTable或从数据库加载DataTable开始是否更改行/如何更改行以及以何种方法更改行
+//当使用Item属性(DataRow索引器)或DataRow对象的GetChildRows方法检索在DataRow中找到的值时,使用DataRowVersion值
+//3.在数据更新过程中如何为DataRow设置新的标准？
+//解答：数据更新过程的最后一步是为DataRow设置新的标准,该操作可用AcceptChanges方法完成,DataAdatapter的Update方法自动调用AcceptChanges
+//4.DataSet中数据更改的信息可以通过两种方式来记录和维护:
+//1.对行的状态作出标记指示该行是否已被修改
+//2.保留DataRow的多个版本,利用这些更改信息,进程可以确定DataSet中有哪些修改并将这些修改发送到数据源
+//5.如何从一个DataSet的表中删除记录？
+//解答：可以使用两个方法从一个DataSet的表中删除记录：DataRowCollection对象的Remove方法和DataRow对象的Delete方法
+//Remove方法从DataRowCollection中删除DataRow,而Delete方法只将行标记为删除
+//DataView对象类似于SQL Server中的视图,它表示DataTable中数据子集的对象
