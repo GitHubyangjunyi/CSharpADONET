b8e313a3eeb3bf2c1829bdf65f1c2578d235c97a
杨俊艺 <1209032479@qq.com>
2018-06-20 08:27:04 +0800

    RowStateVersionDemo最初版本回退点

---
+++ RowStateVersionDemo/FormRowStateVersionDemo.Designer.cs
@@ -0,0 +1,415 @@
+﻿namespace RowStateVersionDemo
+{
+    partial class FormRowStateVersionDemo
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TB_Position = new System.Windows.Forms.TextBox();
+            this.TB_CurrentAddress = new System.Windows.Forms.TextBox();
+            this.TB_CurrentSex = new System.Windows.Forms.TextBox();
+            this.TB_CurrentName = new System.Windows.Forms.TextBox();
+            this.TB_CurrentID = new System.Windows.Forms.TextBox();
+            this.TB_OriginalAddress = new System.Windows.Forms.TextBox();
+            this.TB_OriginalSex = new System.Windows.Forms.TextBox();
+            this.TB_OriginalName = new System.Windows.Forms.TextBox();
+            this.TB_OriginalID = new System.Windows.Forms.TextBox();
+            this.TB_Address = new System.Windows.Forms.TextBox();
+            this.TB_Sex = new System.Windows.Forms.TextBox();
+            this.TB_Name = new System.Windows.Forms.TextBox();
+            this.TB_ID = new System.Windows.Forms.TextBox();
+            this.Rbtn_New = new System.Windows.Forms.RadioButton();
+            this.Rbtn_Modified = new System.Windows.Forms.RadioButton();
+            this.Rbtn_Unchanged = new System.Windows.Forms.RadioButton();
+            this.Btn_Last = new System.Windows.Forms.Button();
+            this.Btn_Forward = new System.Windows.Forms.Button();
+            this.Btn_Next = new System.Windows.Forms.Button();
+            this.Btn_First = new System.Windows.Forms.Button();
+            this.LabelOne = new System.Windows.Forms.Label();
+            this.Btn_Delay = new System.Windows.Forms.Button();
+            this.Btn_Refuse = new System.Windows.Forms.Button();
+            this.Btn_Accept = new System.Windows.Forms.Button();
+            this.Btn_Fill = new System.Windows.Forms.Button();
+            this.Btn_Command = new System.Windows.Forms.Button();
+            this.Btn_Update = new System.Windows.Forms.Button();
+            this.Btn_Edit = new System.Windows.Forms.Button();
+            this.Btn_Delete = new System.Windows.Forms.Button();
+            this.Btn_Insert = new System.Windows.Forms.Button();
+            this.Btn_Save = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TB_Position
+            // 
+            this.TB_Position.Location = new System.Drawing.Point(196, 347);
+            this.TB_Position.Name = "TB_Position";
+            this.TB_Position.Size = new System.Drawing.Size(350, 21);
+            this.TB_Position.TabIndex = 61;
+            // 
+            // TB_CurrentAddress
+            // 
+            this.TB_CurrentAddress.Location = new System.Drawing.Point(482, 247);
+            this.TB_CurrentAddress.Name = "TB_CurrentAddress";
+            this.TB_CurrentAddress.Size = new System.Drawing.Size(100, 21);
+            this.TB_CurrentAddress.TabIndex = 60;
+            // 
+            // TB_CurrentSex
+            // 
+            this.TB_CurrentSex.Location = new System.Drawing.Point(357, 247);
+            this.TB_CurrentSex.Name = "TB_CurrentSex";
+            this.TB_CurrentSex.Size = new System.Drawing.Size(100, 21);
+            this.TB_CurrentSex.TabIndex = 59;
+            // 
+            // TB_CurrentName
+            // 
+            this.TB_CurrentName.Location = new System.Drawing.Point(232, 247);
+            this.TB_CurrentName.Name = "TB_CurrentName";
+            this.TB_CurrentName.Size = new System.Drawing.Size(100, 21);
+            this.TB_CurrentName.TabIndex = 58;
+            // 
+            // TB_CurrentID
+            // 
+            this.TB_CurrentID.Location = new System.Drawing.Point(107, 247);
+            this.TB_CurrentID.Name = "TB_CurrentID";
+            this.TB_CurrentID.Size = new System.Drawing.Size(100, 21);
+            this.TB_CurrentID.TabIndex = 57;
+            // 
+            // TB_OriginalAddress
+            // 
+            this.TB_OriginalAddress.Location = new System.Drawing.Point(482, 204);
+            this.TB_OriginalAddress.Name = "TB_OriginalAddress";
+            this.TB_OriginalAddress.Size = new System.Drawing.Size(100, 21);
+            this.TB_OriginalAddress.TabIndex = 56;
+            // 
+            // TB_OriginalSex
+            // 
+            this.TB_OriginalSex.Location = new System.Drawing.Point(357, 204);
+            this.TB_OriginalSex.Name = "TB_OriginalSex";
+            this.TB_OriginalSex.Size = new System.Drawing.Size(100, 21);
+            this.TB_OriginalSex.TabIndex = 55;
+            // 
+            // TB_OriginalName
+            // 
+            this.TB_OriginalName.Location = new System.Drawing.Point(232, 204);
+            this.TB_OriginalName.Name = "TB_OriginalName";
+            this.TB_OriginalName.Size = new System.Drawing.Size(100, 21);
+            this.TB_OriginalName.TabIndex = 54;
+            // 
+            // TB_OriginalID
+            // 
+            this.TB_OriginalID.Location = new System.Drawing.Point(107, 204);
+            this.TB_OriginalID.Name = "TB_OriginalID";
+            this.TB_OriginalID.Size = new System.Drawing.Size(100, 21);
+            this.TB_OriginalID.TabIndex = 53;
+            // 
+            // TB_Address
+            // 
+            this.TB_Address.Location = new System.Drawing.Point(107, 134);
+            this.TB_Address.Name = "TB_Address";
+            this.TB_Address.Size = new System.Drawing.Size(350, 21);
+            this.TB_Address.TabIndex = 52;
+            // 
+            // TB_Sex
+            // 
+            this.TB_Sex.Location = new System.Drawing.Point(107, 99);
+            this.TB_Sex.Name = "TB_Sex";
+            this.TB_Sex.Size = new System.Drawing.Size(100, 21);
+            this.TB_Sex.TabIndex = 51;
+            // 
+            // TB_Name
+            // 
+            this.TB_Name.Location = new System.Drawing.Point(107, 64);
+            this.TB_Name.Name = "TB_Name";
+            this.TB_Name.Size = new System.Drawing.Size(100, 21);
+            this.TB_Name.TabIndex = 50;
+            // 
+            // TB_ID
+            // 
+            this.TB_ID.Location = new System.Drawing.Point(107, 26);
+            this.TB_ID.Name = "TB_ID";
+            this.TB_ID.Size = new System.Drawing.Size(100, 21);
+            this.TB_ID.TabIndex = 49;
+            // 
+            // Rbtn_New
+            // 
+            this.Rbtn_New.AutoSize = true;
+            this.Rbtn_New.Location = new System.Drawing.Point(309, 299);
+            this.Rbtn_New.Name = "Rbtn_New";
+            this.Rbtn_New.Size = new System.Drawing.Size(47, 16);
+            this.Rbtn_New.TabIndex = 48;
+            this.Rbtn_New.TabStop = true;
+            this.Rbtn_New.Text = "新建";
+            this.Rbtn_New.UseVisualStyleBackColor = true;
+            // 
+            // Rbtn_Modified
+            // 
+            this.Rbtn_Modified.AutoSize = true;
+            this.Rbtn_Modified.Location = new System.Drawing.Point(184, 299);
+            this.Rbtn_Modified.Name = "Rbtn_Modified";
+            this.Rbtn_Modified.Size = new System.Drawing.Size(47, 16);
+            this.Rbtn_Modified.TabIndex = 47;
+            this.Rbtn_Modified.TabStop = true;
+            this.Rbtn_Modified.Text = "修改";
+            this.Rbtn_Modified.UseVisualStyleBackColor = true;
+            // 
+            // Rbtn_Unchanged
+            // 
+            this.Rbtn_Unchanged.AutoSize = true;
+            this.Rbtn_Unchanged.Location = new System.Drawing.Point(59, 299);
+            this.Rbtn_Unchanged.Name = "Rbtn_Unchanged";
+            this.Rbtn_Unchanged.Size = new System.Drawing.Size(59, 16);
+            this.Rbtn_Unchanged.TabIndex = 46;
+            this.Rbtn_Unchanged.TabStop = true;
+            this.Rbtn_Unchanged.Text = "未改变";
+            this.Rbtn_Unchanged.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Last
+            // 
+            this.Btn_Last.Location = new System.Drawing.Point(633, 347);
+            this.Btn_Last.Name = "Btn_Last";
+            this.Btn_Last.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Last.TabIndex = 45;
+            this.Btn_Last.Text = ">>|";
+            this.Btn_Last.UseVisualStyleBackColor = true;
+            this.Btn_Last.Click += new System.EventHandler(this.Btn_Last_Click);
+            // 
+            // Btn_Forward
+            // 
+            this.Btn_Forward.Location = new System.Drawing.Point(552, 347);
+            this.Btn_Forward.Name = "Btn_Forward";
+            this.Btn_Forward.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Forward.TabIndex = 44;
+            this.Btn_Forward.Text = ">";
+            this.Btn_Forward.UseVisualStyleBackColor = true;
+            this.Btn_Forward.Click += new System.EventHandler(this.Btn_Forward_Click);
+            // 
+            // Btn_Next
+            // 
+            this.Btn_Next.Location = new System.Drawing.Point(115, 347);
+            this.Btn_Next.Name = "Btn_Next";
+            this.Btn_Next.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Next.TabIndex = 43;
+            this.Btn_Next.Text = "<";
+            this.Btn_Next.UseVisualStyleBackColor = true;
+            this.Btn_Next.Click += new System.EventHandler(this.Btn_Next_Click);
+            // 
+            // Btn_First
+            // 
+            this.Btn_First.Location = new System.Drawing.Point(34, 346);
+            this.Btn_First.Name = "Btn_First";
+            this.Btn_First.Size = new System.Drawing.Size(75, 25);
+            this.Btn_First.TabIndex = 42;
+            this.Btn_First.Text = "|<<";
+            this.Btn_First.UseVisualStyleBackColor = true;
+            this.Btn_First.Click += new System.EventHandler(this.Btn_First_Click);
+            // 
+            // LabelOne
+            // 
+            this.LabelOne.AutoSize = true;
+            this.LabelOne.Location = new System.Drawing.Point(41, 29);
+            this.LabelOne.Name = "LabelOne";
+            this.LabelOne.Size = new System.Drawing.Size(47, 264);
+            this.LabelOne.TabIndex = 41;
+            this.LabelOne.Text = "学号:\r\n\r\n\r\n姓名:\r\n\r\n\r\n性别:\r\n\r\n\r\n地址:\r\n\r\n\r\n行版本:\r\n\r\n\r\n初始值:\r\n\r\n\r\n当前值:\r\n\r\n\r\n行状态:";
+            // 
+            // Btn_Delay
+            // 
+            this.Btn_Delay.Location = new System.Drawing.Point(633, 288);
+            this.Btn_Delay.Name = "Btn_Delay";
+            this.Btn_Delay.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Delay.TabIndex = 40;
+            this.Btn_Delay.Text = "推迟";
+            this.Btn_Delay.UseVisualStyleBackColor = true;
+            this.Btn_Delay.Click += new System.EventHandler(this.Btn_Delay_Click);
+            // 
+            // Btn_Refuse
+            // 
+            this.Btn_Refuse.Location = new System.Drawing.Point(633, 257);
+            this.Btn_Refuse.Name = "Btn_Refuse";
+            this.Btn_Refuse.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Refuse.TabIndex = 39;
+            this.Btn_Refuse.Text = "拒绝";
+            this.Btn_Refuse.UseVisualStyleBackColor = true;
+            this.Btn_Refuse.Click += new System.EventHandler(this.Btn_Refuse_Click);
+            // 
+            // Btn_Accept
+            // 
+            this.Btn_Accept.Location = new System.Drawing.Point(633, 226);
+            this.Btn_Accept.Name = "Btn_Accept";
+            this.Btn_Accept.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Accept.TabIndex = 38;
+            this.Btn_Accept.Text = "接受";
+            this.Btn_Accept.UseVisualStyleBackColor = true;
+            this.Btn_Accept.Click += new System.EventHandler(this.Btn_Accept_Click);
+            // 
+            // Btn_Fill
+            // 
+            this.Btn_Fill.Location = new System.Drawing.Point(633, 195);
+            this.Btn_Fill.Name = "Btn_Fill";
+            this.Btn_Fill.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Fill.TabIndex = 37;
+            this.Btn_Fill.Text = "填充";
+            this.Btn_Fill.UseVisualStyleBackColor = true;
+            this.Btn_Fill.Click += new System.EventHandler(this.Btn_Fill_Click);
+            // 
+            // Btn_Command
+            // 
+            this.Btn_Command.Location = new System.Drawing.Point(633, 164);
+            this.Btn_Command.Name = "Btn_Command";
+            this.Btn_Command.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Command.TabIndex = 36;
+            this.Btn_Command.Text = "命令";
+            this.Btn_Command.UseVisualStyleBackColor = true;
+            this.Btn_Command.Click += new System.EventHandler(this.Btn_Command_Click);
+            // 
+            // Btn_Update
+            // 
+            this.Btn_Update.Location = new System.Drawing.Point(633, 133);
+            this.Btn_Update.Name = "Btn_Update";
+            this.Btn_Update.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Update.TabIndex = 35;
+            this.Btn_Update.Text = "更新";
+            this.Btn_Update.UseVisualStyleBackColor = true;
+            this.Btn_Update.Click += new System.EventHandler(this.Btn_Update_Click);
+            // 
+            // Btn_Edit
+            // 
+            this.Btn_Edit.Location = new System.Drawing.Point(633, 102);
+            this.Btn_Edit.Name = "Btn_Edit";
+            this.Btn_Edit.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Edit.TabIndex = 34;
+            this.Btn_Edit.Text = "编辑";
+            this.Btn_Edit.UseVisualStyleBackColor = true;
+            this.Btn_Edit.Click += new System.EventHandler(this.Btn_Edit_Click);
+            // 
+            // Btn_Delete
+            // 
+            this.Btn_Delete.Location = new System.Drawing.Point(633, 71);
+            this.Btn_Delete.Name = "Btn_Delete";
+            this.Btn_Delete.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Delete.TabIndex = 33;
+            this.Btn_Delete.Text = "删除";
+            this.Btn_Delete.UseVisualStyleBackColor = true;
+            this.Btn_Delete.Click += new System.EventHandler(this.Btn_Delete_Click);
+            // 
+            // Btn_Insert
+            // 
+            this.Btn_Insert.Location = new System.Drawing.Point(633, 40);
+            this.Btn_Insert.Name = "Btn_Insert";
+            this.Btn_Insert.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Insert.TabIndex = 32;
+            this.Btn_Insert.Text = "添加";
+            this.Btn_Insert.UseVisualStyleBackColor = true;
+            this.Btn_Insert.Click += new System.EventHandler(this.Btn_Insert_Click);
+            // 
+            // Btn_Save
+            // 
+            this.Btn_Save.Location = new System.Drawing.Point(633, 9);
+            this.Btn_Save.Name = "Btn_Save";
+            this.Btn_Save.Size = new System.Drawing.Size(75, 25);
+            this.Btn_Save.TabIndex = 31;
+            this.Btn_Save.Text = "保存";
+            this.Btn_Save.UseVisualStyleBackColor = true;
+            this.Btn_Save.Click += new System.EventHandler(this.Btn_Save_Click);
+            // 
+            // FormRowStateVersionDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.TB_Position);
+            this.Controls.Add(this.TB_CurrentAddress);
+            this.Controls.Add(this.TB_CurrentSex);
+            this.Controls.Add(this.TB_CurrentName);
+            this.Controls.Add(this.TB_CurrentID);
+            this.Controls.Add(this.TB_OriginalAddress);
+            this.Controls.Add(this.TB_OriginalSex);
+            this.Controls.Add(this.TB_OriginalName);
+            this.Controls.Add(this.TB_OriginalID);
+            this.Controls.Add(this.TB_Address);
+            this.Controls.Add(this.TB_Sex);
+            this.Controls.Add(this.TB_Name);
+            this.Controls.Add(this.TB_ID);
+            this.Controls.Add(this.Rbtn_New);
+            this.Controls.Add(this.Rbtn_Modified);
+            this.Controls.Add(this.Rbtn_Unchanged);
+            this.Controls.Add(this.Btn_Last);
+            this.Controls.Add(this.Btn_Forward);
+            this.Controls.Add(this.Btn_Next);
+            this.Controls.Add(this.Btn_First);
+            this.Controls.Add(this.LabelOne);
+            this.Controls.Add(this.Btn_Delay);
+            this.Controls.Add(this.Btn_Refuse);
+            this.Controls.Add(this.Btn_Accept);
+            this.Controls.Add(this.Btn_Fill);
+            this.Controls.Add(this.Btn_Command);
+            this.Controls.Add(this.Btn_Update);
+            this.Controls.Add(this.Btn_Edit);
+            this.Controls.Add(this.Btn_Delete);
+            this.Controls.Add(this.Btn_Insert);
+            this.Controls.Add(this.Btn_Save);
+            this.Name = "FormRowStateVersionDemo";
+            this.Text = "RowStateVersionDemo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TB_Position;
+        private System.Windows.Forms.TextBox TB_CurrentAddress;
+        private System.Windows.Forms.TextBox TB_CurrentSex;
+        private System.Windows.Forms.TextBox TB_CurrentName;
+        private System.Windows.Forms.TextBox TB_CurrentID;
+        private System.Windows.Forms.TextBox TB_OriginalAddress;
+        private System.Windows.Forms.TextBox TB_OriginalSex;
+        private System.Windows.Forms.TextBox TB_OriginalName;
+        private System.Windows.Forms.TextBox TB_OriginalID;
+        private System.Windows.Forms.TextBox TB_Address;
+        private System.Windows.Forms.TextBox TB_Sex;
+        private System.Windows.Forms.TextBox TB_Name;
+        private System.Windows.Forms.TextBox TB_ID;
+        private System.Windows.Forms.RadioButton Rbtn_New;
+        private System.Windows.Forms.RadioButton Rbtn_Modified;
+        private System.Windows.Forms.RadioButton Rbtn_Unchanged;
+        private System.Windows.Forms.Button Btn_Last;
+        private System.Windows.Forms.Button Btn_Forward;
+        private System.Windows.Forms.Button Btn_Next;
+        private System.Windows.Forms.Button Btn_First;
+        private System.Windows.Forms.Label LabelOne;
+        private System.Windows.Forms.Button Btn_Delay;
+        private System.Windows.Forms.Button Btn_Refuse;
+        private System.Windows.Forms.Button Btn_Accept;
+        private System.Windows.Forms.Button Btn_Fill;
+        private System.Windows.Forms.Button Btn_Command;
+        private System.Windows.Forms.Button Btn_Update;
+        private System.Windows.Forms.Button Btn_Edit;
+        private System.Windows.Forms.Button Btn_Delete;
+        private System.Windows.Forms.Button Btn_Insert;
+        private System.Windows.Forms.Button Btn_Save;
+    }
+}
+
---
+++ RowStateVersionDemo/FormRowStateVersionDemo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace RowStateVersionDemo
+{
+    public partial class FormRowStateVersionDemo : Form
+    {
+        public FormRowStateVersionDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Save_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Insert_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Delete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Edit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Update_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Command_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Fill_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Accept_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Refuse_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Delay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_First_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Next_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Forward_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Last_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
